68e59ea54a535b187314ec6d698c8bfc4e18a860
thek <soft.thek@gmail.com>
2024-06-16 20:06:16 +0200

    issue #5 unchecked sll implementation test

--- lists/tests/src/tests.c
+++ lists/tests/src/tests.c
@@ -1,8 +1,13 @@
 #include "tests.h"
+#include "tests_sll.h"
 #include "tests_sll_s.h"
 #include "tests_dll_s.h"
 
 const test_suite_getter_t tsg_table[] = {
+  get_sll_node_test,
+  get_sll_list_base_test,
+  get_sll_list_manage_test,
+  get_sll_list_traverse_test,
   get_sll_s_node_test,
   get_sll_s_list_base_test,
   get_sll_s_list_manage_test,
---
+++ lists/tests/src/tests_sll.h
@@ -0,0 +1,11 @@
+#ifndef _TESTS_SLL_H_
+#define _TESTS_SLL_H_
+
+#include "tests.h"
+
+test_suite_t* get_sll_node_test();
+test_suite_t* get_sll_list_base_test();
+test_suite_t* get_sll_list_manage_test();
+test_suite_t* get_sll_list_traverse_test();
+
+#endif
